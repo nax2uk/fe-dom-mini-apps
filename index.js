a1a459162bae13888db2b1b8648684c9adf09c74
nax2uk <azlinayeo@gmail.com>
2020-04-20 20:29:27 +0100

    added deadline to task

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const taskList = document.getElementById('listTasks');
-const arrTasks = document.getElementsByClassName("tasks");
 const btnAddTask = document.getElementById('addTask');
 const txtNewTask = document.getElementById('newTask');
+const dateDeadline = document.getElementById('deadline');
+const arrTasks = document.getElementsByClassName("tasks");
+
 const jsTasks = [...arrTasks];
 
+console.dir(dateDeadline);
+
+const formatDate = (strDate) => {
+  const elem = strDate.split(' ');
+  return `${elem[0]} ${elem[1]} ${elem[2]}`
+}
 
 const handleClickTask = (clickEvent) => {
   //add a check mark in front of text if class is not equals completed
@@ -13,11 +21,20 @@ const handleClickTask = (clickEvent) => {
 
 const handleSubmitNewTask = () => {
   const newTask = document.createElement('li');
+  const deadline = document.createElement('time');
+
   newTask.setAttribute('class', 'tasks');
   newTask.innerText = txtNewTask.value;
   newTask.addEventListener('click', handleClickTask);
+
+  deadline.setAttribute('datetime', dateDeadline.value);
+  deadline.innerText = formatDate(dateDeadline.valueAsDate.toString());
+
+  newTask.appendChild(deadline);
   taskList.appendChild(newTask);
+
   txtNewTask.value = "";
+  dateDeadline.value = "";
 }
 
 btnAddTask.addEventListener('click', handleSubmitNewTask);
